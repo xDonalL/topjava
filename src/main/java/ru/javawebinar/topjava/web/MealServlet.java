4bfe26cbc6de6e83926d1654b0f2d17811e61163
Yuriy Subbotin <Zencer1335@gmail.com>
2024-06-10 13:21:36 +0300

    add meals.jsp

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,13 +1,33 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(MealServlet.class);
+    private final List<Meal> meals = MealsUtil.meals;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        log.debug("redirecting to meal");
+        List<MealTo> mealsTo = meals.stream()
+                .map(meal ->
+                        new MealTo(meal.getDateTime(), meal.getDescription(),
+                                meal.getCalories(), true))
+                .collect(Collectors.toList());
+        request.setAttribute("meals", mealsTo);
+        request.getRequestDispatcher("meals.jsp").forward(request, response);
     }
 
     @Override
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,29 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meals</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+<p></p>
+<table border="1" cellpadding="4" cellspacing="0">
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th></th>
+        <th></th>
+    </tr>
+    <c:forEach items="${meals}" var="meal">
+        <tr>
+            <td>${meal.dateTime}</td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
