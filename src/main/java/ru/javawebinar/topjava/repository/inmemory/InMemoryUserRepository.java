320f32121f8477154c0e4f1caf6de4c1e628d5c0
Yuriy Subbotin <Zencer1335@gmail.com>
2025-05-07 15:03:55 +0300

    add getAll sorted

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -48,7 +45,9 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return new ArrayList<>(users.values());
+        return users.values().stream()
+                .sorted(Comparator.comparing(User::getName).thenComparing((User::getEmail)))
+                .collect(Collectors.toList());
     }
 
     @Override
